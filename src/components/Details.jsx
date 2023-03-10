f28bce5d80dabf26cf9084aceeb65877f3cce2d2
Utkarsh Patel <pateluttkarsh697@gmail.com>
2023-03-10 20:09:12 +0530

    modified the search bar

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -4,12 +4,16 @@ export default function Details() {
 
     return (
         <div className='w-1/4 h-[100vh] absolute top-0 right-0 opacity-60 z-0 bg-gray-800'>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
             <div className='flex'>
-                <input type="text" defaultValue={"Search Loaction"} className='w-[80%] bg-gray-800' />
-                <span class="material-symbols-outlined px-[5%] py-4 bg-cyan-200 text-black" style={{fontSize:"60px"}}>
+                <input type="text" defaultValue={"Search Loaction"} className='w-[83%] ml-[5%] text-gray-400 text-lg my-9 bg-gray-800 outline-0 border-b-2 border-gray-400' />
+
+                <span class="material-symbols-outlined pt-[2.5vh] h-[9vh] justify-center text-center w-[5vw] bg-cyan-200 items-center text-black" style={{ fontSize: "30px" }}>
                     search
                 </span>
+
+
             </div>
         </div>
     )
